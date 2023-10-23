205f64d48d3ed354b1eb32a4358f69269324132c
dev-null-undefined <martinkos007@gmail.com>
2023-10-23 21:20:10 +0200

    init: basic flake.nix to generate HTML presentation with reveal.js

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    revealjs =
+      # nix run nixpkgs#nurl -- https://github.com/hakimel/reveal.js 4.6.1
+      pkgs.fetchFromGitHub {
+        owner = "hakimel";
+        repo = "reveal.js";
+        rev = "4.6.1";
+        hash = "sha256-R599Zdw9YzID7CRzWRcasz+ZZvZiagBFsuDWUvnNX4o=";
+      };
+  in {
+    packages.x86_64-linux = {
+      presentation = pkgs.runCommand "intro-presentation" {} ''
+        mkdir $out
+        ln -s ${revealjs} $out/reveal.js
+        cp ${./index.html} $out/index.html
+      '';
+    };
+  };
+}

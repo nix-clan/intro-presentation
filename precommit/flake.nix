3bb2a92df28109167c843580fc5f1f4af5f3185a
Michal Atlas <michal_atlas+git@posteo.net>
2023-10-25 15:50:48 +0200

    Precommit: Add demo

---
+++ precommit/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    nixpkgs.url = "github:NixOs/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, pre-commit-hooks, ... }:
+    let system = "x86_64-linux"; in
+    {
+      checks.${system} = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true;
+          };
+        };
+      };
+      devShell.${system} = nixpkgs.legacyPackages.${system}.mkShell {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+      };
+    };
+}

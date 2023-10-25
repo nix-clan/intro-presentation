6f547024dfa8fd1bc4d0596a3b7791cbadd5e431
Michal Atlas <michal_atlas+git@posteo.net>
2023-10-25 19:57:56 +0200

    Pkgs: remove devshell part

--- pkg/flake.nix
+++ pkg/flake.nix
@@ -17,18 +17,5 @@
           ${system}.mystic = import ./pkgs/mystic.nix pkgs;
         }
         another-flake.packages;
-      devShells.${system}.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          python3
-          nixfmt
-          (pkgs.writeShellScriptBin "foobar" ''
-            echo 'Welcome to the FIT Nix meetup'
-          '')
-          (pkgs.writeShellScriptBin "my-old-python" ''
-            ${pkgs.python38}/bin/python "$@"
-          '')
-          pkgs.hello.nativeBuildInputs
-        ];
-      };
     };
 }

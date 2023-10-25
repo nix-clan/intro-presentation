17ec488998f51d817f669c42f2339b5816120a88
Michal Atlas <michal_atlas+git@posteo.net>
2023-10-25 08:53:51 +0200

    Direnv: Add demo files

---
+++ direnv/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          python3
+          nixfmt
+          (pkgs.writeShellScriptBin "foobar" ''
+            echo 'Welcome to the FIT Nix meetup'
+          '')
+          (pkgs.writeShellScriptBin "my-old-python" ''
+            ${pkgs.python38}/bin/python "$@"
+          '')
+          pkgs.hello.nativeBuildInputs
+        ];
+      };
+    };
+}
